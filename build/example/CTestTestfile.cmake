b058aa52440d9445ecc7bc11c5e49a8ff0579b3b
mcong <max.cong@nokia-sbell.com>
2017-08-17 10:05:55 +0800

    delete build lib

--- build/example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mcong/study/github/RedisSim/example
-# Build directory: /home/mcong/study/github/RedisSim/build/example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mcong/study/github/RedisSim/src
-# Build directory: /home/mcong/study/github/RedisSim/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
